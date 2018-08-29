f31e6647f9fdc56f224fd6f1d6c95d0f1af27ac6
Chris Jewell <chrisjohnjewell@gmail.com>
2018-08-29 21:02:54 +1000

    Add basic ncurses Printer

--- lib/coin_flip_command_line_game/printer.ex
+++ lib/coin_flip_command_line_game/printer.ex
@@ -1,8 +1,27 @@
 defmodule CoinFlipCommandLineGame.Printer do
-  def init(_ \\ nil), do: :ok
-  def teardown(_ \\ nil), do: :ok
+  alias CoinFlipCommandLineGame.Screen
 
-  def print_full_screen(string) do
-    IO.puts(string)
+  def init(_ \\ nil) do
+    ExNcurses.initscr()
+    ExNcurses.listen()
+    ExNcurses.noecho()
+    ExNcurses.keypad()
+    ExNcurses.curs_set(0)
+
+    :ok
+  end
+
+  def teardown(_ \\ nil) do
+    ExNcurses.stop_listening()
+    ExNcurses.endwin()
+  end
+
+  def print_full_screen(%Screen{user_buffer: user_buffer, display: display, prompt: prompt}) do
+    ExNcurses.clear()
+    ExNcurses.mvaddstr(0, 0, prompt)
+    ExNcurses.addstr(user_buffer)
+    ExNcurses.mvaddstr(2, 0, display)
+
+    ExNcurses.refresh()
   end
 end
